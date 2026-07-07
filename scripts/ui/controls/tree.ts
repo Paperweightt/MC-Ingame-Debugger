5ab7dba77101a4ca543e433e024f60d70c6800db
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-07-06 21:17:09 -0400

    refactor: seperate out addNode

--- scripts/ui/controls/tree.ts
+++ scripts/ui/controls/tree.ts
@@ -17,41 +17,32 @@ export class TreeLayout extends Node {
     const childPrefix = prefix + "│ ";
     let state = false;
 
+    function addNode(key: string | number, value: any): void {
+      if (typeof value === "object") {
+        branch.add(new TreeLayout("" + key, value, childPrefix));
+      } else if (typeof value === "function") {
+        branch.add(new Button(childPrefix + "ƒ " + key));
+      } else {
+        branch.add(new Button(childPrefix + "≡ " + key + ": " + value));
+      }
+    }
+
     const button = new Button(prefix + "> " + key).setOnClick((ctx) => {
       if (!state) {
         if (value instanceof Set) {
           let key = 0;
-
           for (const node of value) {
-            if (typeof node === "object") {
-              branch.add(new TreeLayout("" + key, node, childPrefix));
-            } else if (typeof node === "function") {
-              branch.add(new Button(childPrefix + "ƒ " + key));
-            } else {
-              branch.add(new Button(childPrefix + "≡ " + key + ": " + node));
-            }
+            addNode(key, node);
             key++;
           }
         }
         if (value instanceof Map) {
           for (const [key, node] of value) {
-            if (typeof node === "object") {
-              branch.add(new TreeLayout(key, node, childPrefix));
-            } else if (typeof node === "function") {
-              branch.add(new Button(childPrefix + "ƒ " + key));
-            } else {
-              branch.add(new Button(childPrefix + "≡ " + key + ": " + node));
-            }
+            addNode(key, node);
           }
         } else {
           for (const [key, node] of Object.entries(value)) {
-            if (typeof node === "object") {
-              branch.add(new TreeLayout(key, node, childPrefix));
-            } else if (typeof node === "function") {
-              branch.add(new Button(childPrefix + "ƒ " + key));
-            } else {
-              branch.add(new Button(childPrefix + "≡ " + key + ": " + node));
-            }
+            addNode(key, node);
           }
         }
 

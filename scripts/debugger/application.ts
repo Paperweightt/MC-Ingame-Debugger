db687ee5f4cc454898abc3a8d500d5fcdb58fc21
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-07-06 21:11:50 -0400

    feat: tree layout

--- scripts/debugger/application.ts
+++ scripts/debugger/application.ts
@@ -1,26 +1,27 @@
 import { Dimension, Player, system, Vector3, world } from "@minecraft/server";
 import { Root } from "../ui/scene/root";
 import { VerticalLayout } from "../ui/layout/verticalLayout";
-import { Button } from "../ui/controls/button";
 import { TreeLayout } from "../ui/controls/tree";
-
-system.runInterval(() => {
-  for (const [_, app] of Application.list) {
-    app.root.hover(app.owner);
-  }
-});
-
-world.afterEvents.playerSwingStart.subscribe((event) => {
-  const { player } = event;
-  const app = Application.list.get(player.id);
-
-  if (!app) return;
-
-  app.root.click(player);
-});
 
 export class Application {
   static list: Map<string, Application> = new Map();
+
+  static {
+    system.runInterval(() => {
+      for (const [_, app] of Application.list) {
+        app.root.hover(app.owner);
+      }
+    });
+
+    world.afterEvents.playerSwingStart.subscribe((event) => {
+      const { player } = event;
+      const app = Application.list.get(player.id);
+
+      if (!app) return;
+
+      app.root.click(player);
+    });
+  }
 
   root: Root;
 
@@ -36,20 +37,9 @@ export class Application {
   }
 
   build(): void {
-    const words = ["hi", "hello", "wahhhh"];
     const sidebar = new VerticalLayout();
 
-    for (const word of words) {
-      sidebar.add(
-        new Button(word).setOnClick((ctx) => {
-          ctx.player.sendMessage(word);
-        })
-      );
-    }
-
-    sidebar.add(
-      new TreeLayout("location", { x: { x: { x: 0, y: 0, z: 0 }, y: 0, z: 0 }, y: 0, z: 0 })
-    );
+    sidebar.add(new TreeLayout("debugger", this.root));
 
     this.root.add(sidebar);
     this.root.frame();
--- scripts/ui/controls/tree.ts
+++ scripts/ui/controls/tree.ts
@@ -7,18 +7,52 @@ export class TreeLayout extends Node {
 
   constructor(
     public key: string,
-    public value: Object
+    public value: Object,
+    public prefix: string = ""
   ) {
     super();
     this.add(this.layout);
 
     const branch = new VerticalLayout();
+    const childPrefix = prefix + "│ ";
     let state = false;
 
-    const button = new Button(key).setOnClick((ctx) => {
+    const button = new Button(prefix + "> " + key).setOnClick((ctx) => {
       if (!state) {
-        for (const [key, node] of Object.entries(value)) {
-          branch.add(new TreeLayout(key, node));
+        if (value instanceof Set) {
+          let key = 0;
+
+          for (const node of value) {
+            if (typeof node === "object") {
+              branch.add(new TreeLayout("" + key, node, childPrefix));
+            } else if (typeof node === "function") {
+              branch.add(new Button(childPrefix + "ƒ " + key));
+            } else {
+              branch.add(new Button(childPrefix + "≡ " + key + ": " + node));
+            }
+            key++;
+          }
+        }
+        if (value instanceof Map) {
+          for (const [key, node] of value) {
+            if (typeof node === "object") {
+              branch.add(new TreeLayout(key, node, childPrefix));
+            } else if (typeof node === "function") {
+              branch.add(new Button(childPrefix + "ƒ " + key));
+            } else {
+              branch.add(new Button(childPrefix + "≡ " + key + ": " + node));
+            }
+          }
+        } else {
+          for (const [key, node] of Object.entries(value)) {
+            if (typeof node === "object") {
+              branch.add(new TreeLayout(key, node, childPrefix));
+            } else if (typeof node === "function") {
+              branch.add(new Button(childPrefix + "ƒ " + key));
+            } else {
+              branch.add(new Button(childPrefix + "≡ " + key + ": " + node));
+            }
+          }
         }
 
         this.layout.add(branch);
@@ -26,7 +60,6 @@ export class TreeLayout extends Node {
         branch.clear();
         this.layout.remove(branch);
       }
-
       ctx.frame();
 
       state = !state;

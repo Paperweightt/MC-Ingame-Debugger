3e1ee30201ce8ff66d87133927e01a2de45af2f6
Paperweightt <123002094+Paperweightt@users.noreply.github.com>
2026-07-07 22:18:08 -0400

    feat: tree arrow toggles between state

--- scripts/ui/controls/tree.ts
+++ scripts/ui/controls/tree.ts
@@ -1,6 +1,8 @@
+import { colorCodes } from "../../utils";
 import { VerticalLayout } from "../layout/verticalLayout";
 import { Node } from "../scene/node";
 import { Button } from "./button";
+import { Label } from "./label";
 
 export class TreeLayout extends Node {
   layout = new VerticalLayout();
@@ -27,33 +29,41 @@ export class TreeLayout extends Node {
       }
     }
 
-    const button = new Button(prefix + "> " + key).setOnClick((ctx) => {
-      if ((state = !state)) {
-        branch.clear();
+    const label = new Label(prefix + "> " + key);
 
-        if (value instanceof Set) {
-          let key = 0;
-          for (const node of value) {
-            addNode(key, node);
-            key++;
+    const button = new Button()
+      .setOnClick((ctx) => {
+        if ((state = !state)) {
+          branch.clear();
+
+          if (value instanceof Set) {
+            let key = 0;
+            for (const node of value) {
+              addNode(key, node);
+              key++;
+            }
+          } else if (value instanceof Map) {
+            for (const [key, node] of value) addNode(key, node);
+          } else {
+            for (const [key, node] of Object.entries(value)) addNode(key, node);
           }
-        }
-        if (value instanceof Map) {
-          for (const [key, node] of value) {
-            addNode(key, node);
-          }
+
+          label.string = prefix + "v " + key;
+          this.layout.add(branch);
         } else {
-          for (const [key, node] of Object.entries(value)) {
-            addNode(key, node);
-          }
+          label.string = prefix + "> " + key;
+          this.layout.remove(branch);
         }
+        ctx.frame();
+      })
+      .setOnHover(() => {
+        label.textPrimitive?.setText(colorCodes.gray + label.string);
+      })
+      .setOnHoverEnd(() => {
+        label.textPrimitive?.setText(label.string);
+      });
 
-        this.layout.add(branch);
-      } else {
-        this.layout.remove(branch);
-      }
-      ctx.frame();
-    });
+    button.add(label);
 
     this.layout.add(button);
   }
